0297ade32893c32539b9e8c63d2c11e95a884ddc
Tracy McAbee <tracy@mcabeehome.com>
2016-03-03 14:12:45 -0500

    Checking for 2 different trips

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ruby southwest.rb --orig "ATL" --dest "LAX" --depart_date "07/22/2016" --return_date "07/30/2016"
+ruby southwest.rb --orig "ATL" --dest "LAX" --depart_date "07/21/2016" --return_date "07/31/2016"
